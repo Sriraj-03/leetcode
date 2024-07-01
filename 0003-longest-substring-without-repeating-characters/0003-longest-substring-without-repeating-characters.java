5a7bd07a5a2f8a638d1032bf093ab2ac77a7db30
Sriraj-03 <rocksri2k@gmail.com>
2024-07-01 19:52:30 +0530

    Time: 2 ms (98.92%), Space: 42 MB (99.96%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,21 +1,42 @@
 import java.util.HashSet;
 
 public class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        HashSet<Character> set = new HashSet<>();
-        int longest = 0, i = 0, j = 0;
+    // public int lengthOfLongestSubstring(String s) {
+    //     HashSet<Character> set = new HashSet<>();
+    //     int longest = 0, i = 0, j = 0;
         
+    //     while (j < s.length()) {
+    //         if (!set.contains(s.charAt(j))) {
+    //             set.add(s.charAt(j));
+    //             j++;
+    //             longest = Math.max(longest, set.size());
+    //         } else {
+    //             set.remove(s.charAt(i));
+    //             i++;
+    //         }
+    //     }
+        
+    //     return longest;
+    // }
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) return 0;
+
+        boolean[] seen = new boolean[256]; // Assuming ASCII characters
+        int longest = 0, i = 0, j = 0;
+
         while (j < s.length()) {
-            if (!set.contains(s.charAt(j))) {
-                set.add(s.charAt(j));
+            if (!seen[s.charAt(j)]) {
+                seen[s.charAt(j)] = true;
                 j++;
-                longest = Math.max(longest, set.size());
+                longest = Math.max(longest, j - i);
             } else {
-                set.remove(s.charAt(i));
+                seen[s.charAt(i)] = false;
                 i++;
             }
         }
-        
+
         return longest;
     }
+
 }
