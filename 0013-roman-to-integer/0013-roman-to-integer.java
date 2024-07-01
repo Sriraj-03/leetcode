f21be7edf7903a660281a639e7f692ecfa951da5
Sriraj-03 <rocksri2k@gmail.com>
2024-07-01 20:55:03 +0530

    Time: 2 ms (100%), Space: 44.4 MB (71.97%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -37,3 +37,39 @@ class Solution {
 // values[12] = 1000; // 'M'
 // values[21] = 5;   // 'V'
 // values[23] = 10;  // 'X'
+// class Solution {
+//     public int getValue(char ch)
+//     {
+//         switch(ch)
+//         {
+//             case 'I': return 1;
+//             case 'V': return 5;
+//             case 'X': return 10;
+//             case 'L': return 50;
+//             case 'C': return 100;
+//             case 'D': return 500;
+//             case 'M': return 1000; 
+//             default : return 0;
+//         }
+//     }
+
+//     public int romanToInt(String s) {
+//         int sum=0;
+//         int n=s.length();
+//         for(int i=0;i<n;i++)
+//         {
+//            char ch1=s.charAt(i);
+
+//            if((i+1)<n && getValue(ch1)<getValue(s.charAt(i+1)))
+//            {
+//             sum=sum-getValue(ch1);
+//            }
+//            else
+//            {
+//             sum=sum+getValue(ch1);
+//            }
+
+//         }
+
+//         return sum;
+//     }
