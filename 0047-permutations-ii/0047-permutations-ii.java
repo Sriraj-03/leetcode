5109bc3c46608942f27259902fe20082f31edbb3
Sriraj-03 <rocksri2k@gmail.com>
2024-07-02 20:02:32 +0530

    Time: 159 ms (14.21%), Space: 44.5 MB (80.44%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -22,3 +22,41 @@ class Solution {
         }
     }
 }
+
+// class Solution {
+//     public List<List<Integer>> permuteUnique(int[] nums) {
+//       Set<List<Integer>>ans=new HashSet<>();
+//         List<Integer>res=new ArrayList<>();
+        
+//       boolean arr[]=new boolean[nums.length];
+     
+//         Permu(nums,arr,ans,res,0);
+//         return new ArrayList<>(ans);
+    
+    
+
+//     }
+//     public static void Permu(int[]nums,boolean arr[], Set<List<Integer>>ans,List<Integer>res,int idx){
+// if(idx==nums.length){
+//     ans.add(new ArrayList<>(res));
+//     return;
+// }
+
+
+// for(int i=0;i<nums.length;i++){
+//     if(arr[i]==false){
+//     res.add(nums[i]);
+//     arr[i]=true;
+    
+//     Permu(nums,arr,ans,res,idx+1);
+//     res.remove(res.size()-1);
+//     arr[i]=false;
+   
+//     }
+//  }
+
+
+// }
+ 
+
+//     }
