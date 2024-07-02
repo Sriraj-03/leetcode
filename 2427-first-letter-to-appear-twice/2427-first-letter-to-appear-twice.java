2c4e4baf5ab661a1abbc11a8950d0a72ffd9df79
Sriraj-03 <rocksri2k@gmail.com>
2024-07-02 12:54:41 +0530

    Time: 0 ms (100%), Space: 41.3 MB (68.11%) - LeetHub

---
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.java
@@ -0,0 +1,11 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        int [] has=new int[26];
+        for(int i=0;i<s.length();i++){
+            has[s.charAt(i)-'a']++;
+            if( has[s.charAt(i)-'a']==2)
+            return s.charAt(i);
+        }
+        return 'z';
+    }
+}
