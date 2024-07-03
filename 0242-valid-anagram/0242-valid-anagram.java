ed6d5e776b57dc32ce4a3ce20d3c149377f4bb8b
Sriraj-03 <rocksri2k@gmail.com>
2024-07-03 11:37:11 +0530

    Time: 4 ms (70.53%), Space: 42.5 MB (95.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,34 +1,34 @@
-// class Solution {
-//     public boolean isAnagram(String s, String t) {
-//         if(s.length()!=t.length())
-//         return false;
-//         else{
-//             int[] has1= new int[26];
-//              int[] has2= new int[26];
-//         for(int i=0;i<s.length();i++){
-//                 has1[s.charAt(i)-'a']++;
-//                 has2[t.charAt(i)-'a']++;
-//             }
-//         for(int i=0;i<s.length();i++){
-//             if(has1[s.charAt(i)-'a']!=has2[s.charAt(i)-'a'])
-//             return false;
-//         }
-//         return true;
-//         }
-        
-//     }
-// }
-
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        char[]x=s.toCharArray();
-        char[]y=t.toCharArray();
-        Arrays.sort(x);
-        Arrays.sort(y);
-        for(int i=0;i<x.length;i++){
-            if(x[i]!=y[i]) return false;
+        if(s.length()!=t.length())
+        return false;
+        else{
+            int[] has1= new int[26];
+             int[] has2= new int[26];
+        for(int i=0;i<s.length();i++){
+                has1[s.charAt(i)-'a']++;
+                has1[t.charAt(i)-'a']--;
+            }
+        for(int i=0;i<s.length();i++){
+            if(has1[s.charAt(i)-'a']!=0)
+            return false;
         }
-
-  return true;  }
+        return true;
+        }
+        
+    }
 }
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length()!=t.length()) return false;
+//         char[]x=s.toCharArray();
+//         char[]y=t.toCharArray();
+//         Arrays.sort(x);
+//         Arrays.sort(y);
+//         for(int i=0;i<x.length;i++){
+//             if(x[i]!=y[i]) return false;
+//         }
+
+//   return true;  }
+// }
