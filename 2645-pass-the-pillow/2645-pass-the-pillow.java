fa8ece5e4e44c543908add92dbbbed375039b7a9
Sriraj-03 <rocksri2k@gmail.com>
2024-07-06 09:49:05 +0530

    Time: 0 ms (100%), Space: 40 MB (74.29%) - LeetHub

---
+++ 2645-pass-the-pillow/2645-pass-the-pillow.java
@@ -0,0 +1,41 @@
+// class Solution{
+//     public int passThePillow(int n,int time)
+//     {
+
+//     }
+// }
+
+class Solution {
+    public int passThePillow(int n, int time) {
+        int cnt=1,dem=n;
+        while(time>0){
+                if(cnt==n)
+                cnt=cnt*-1;
+                if(cnt==-1)
+                cnt=1;
+                cnt++;
+            time--;
+        }
+        return Math.abs(cnt);
+    }
+}
+
+// // class Solution {
+// //     public int passThePillow(int n, int time) {
+// //         int cnt=1,dem=n;;
+// //         while(time>0){
+// //                 if(time==1){
+// //                     return Math.abs(cnt);
+// //                 }
+// //                 cnt++;
+// //                 if(cnt==n)
+// //                 cnt=-cnt;
+               
+// //                 if(cnt==0)
+// //                 cnt=1;
+                
+// //             time--;
+// //         }
+// //         return -1;
+// //     }
+// // }
