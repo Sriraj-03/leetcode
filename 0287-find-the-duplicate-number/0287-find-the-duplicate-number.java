f33088288eeb9f533509719231c4a09e7a5dc3a1
Sriraj-03 <rocksri2k@gmail.com>
2024-07-08 13:56:41 +0530

    Time: 2 ms (97.16%), Space: 58.5 MB (18.03%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,13 +1,25 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-       
-        int[] hash=new int[100000+1];
+        boolean res[]= new boolean[nums.length];
         for(int i=0;i<nums.length;i++){
-                hash[nums[i]]++;
-                if(hash[nums[i]]>1){
-                    return nums[i];
-                }
+            if(res[nums[i]])
+            return nums[i];
+
+            res[nums[i]]=true;
         }
         return 0;
     }
 }
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+       
+//         int[] hash=new int[100000+1];
+//         for(int i=0;i<nums.length;i++){
+//                 hash[nums[i]]++;
+//                 if(hash[nums[i]]>1){
+//                     return nums[i];
+//                 }
+//         }
+//         return 0;
+//     }
+// }
