3fc55c1b2edd1afede34f6668e10c16ab843ba37
Sriraj-03 <rocksri2k@gmail.com>
2024-07-08 14:15:08 +0530

    Time: 2 ms (98.35%), Space: 46.6 MB (88.29%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -39,6 +39,63 @@ class Solution {
         return dag;
     }
 }
+// class Solution { 
+//     private int mIndex;
+//     private int nIndex;
+//     private int index;
+    
+//     public int[] findDiagonalOrder(int[][] mat) {
+//         int m = mat.length;
+//         int n = mat[0].length;
+//         int[] arr = new int[m*n];
+        
+//         if(m == 1){
+//             return mat[0];
+//         }
+//         if(n == 1){
+//             for(int i = 0; i < m; i++){
+//                 arr[i] = mat[i][0];
+//             }
+//             return arr;
+//         }
+        
+//         mIndex = 0;
+//         nIndex = 0;
+//         index = 0;
+//         while(index < arr.length - 1){
+//             traverseForward(mat, m , n, arr);
+//             traverseBack(mat, m, n, arr);
+//         }
+//         arr[index] = mat[m - 1][n - 1];
+//         return arr;
+//     }
+    
+//     public void traverseForward(int[][] mat, int m, int n, int[] arr){
+//         while(mIndex > -1 && nIndex < n && index < arr.length - 1){
+//             arr[index++] = mat[mIndex--][nIndex++];
+//         }
+//         if(nIndex == n){
+//             mIndex += 2;
+//             nIndex--;
+//         }
+//         else if(mIndex == -1){
+//             mIndex++;
+//         }
+//     }
+    
+//     public void traverseBack(int[][] mat, int m, int n, int[] arr){
+//         while(mIndex < m && nIndex > -1 && index < arr.length - 1){
+//             arr[index++] = mat[mIndex++][nIndex--];
+//         }
+//         if(mIndex == m){
+//             nIndex += 2;
+//             mIndex--;
+//         }
+//         else if(nIndex == -1){
+//             nIndex++;
+//         }
+//     }
+// }
 
 // class Solution {
 //     public int[] findDiagonalOrder(int[][] mat) {
