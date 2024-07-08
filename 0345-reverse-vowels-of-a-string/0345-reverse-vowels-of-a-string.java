4b6054c1be87ba6e2e9207169997d55701353658
Sriraj-03 <rocksri2k@gmail.com>
2024-07-08 14:02:49 +0530

    Time: 3 ms (80.82%), Space: 46.1 MB (8.79%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,26 +1,69 @@
+// class Solution {
+//     public String reverseVowels(String s) {
+//         char[] a= s.toCharArray();
+//         int l=a.length;
+//         int c=0;
+//         char[] b= new char[l];
+//         for(int i=0;i<l;i++){
+            
+//             if(a[i]=='a'||a[i]=='e'||a[i]=='o'||a[i]=='i'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='O'||a[i]=='I'||a[i]=='U'){
+//                     b[c]=a[i];
+//                     c++;
+//                     a[i]='^';
+//             }
+//         }
+       
+//          c=0;
+//         for(int i=l-1;i>=0;i--){
+//             if(a[i]=='^'){
+//                 a[i]=b[c];
+//                 c++;
+//             }
+//         }
+//        String se= new String(a);
+//        return se;
+//     }
+// }
+
 class Solution {
     public String reverseVowels(String s) {
-        char[] a= s.toCharArray();
-        int l=a.length;
-        int c=0;
-        char[] b= new char[l];
-        for(int i=0;i<l;i++){
-            
-            if(a[i]=='a'||a[i]=='e'||a[i]=='o'||a[i]=='i'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='O'||a[i]=='I'||a[i]=='U'){
-                    b[c]=a[i];
-                    c++;
-                    a[i]='^';
+        char[] a = s.toCharArray();
+        int left = 0;
+        int right = a.length - 1;
+
+        while (left < right) {
+            // Find the next vowel from the left.
+            while (left < right && !isVowel(a[left])) {
+                left++;
+            }
+            // Find the next vowel from the right.
+            while (left < right && !isVowel(a[right])) {
+                right--;
+            }
+            // Swap the vowels.
+            if (left < right) {
+                char temp = a[left];
+                a[left] = a[right];
+                a[right] = temp;
+                left++;
+                right--;
             }
         }
-       
-         c=0;
-        for(int i=l-1;i>=0;i--){
-            if(a[i]=='^'){
-                a[i]=b[c];
-                c++;
-            }
-        }
-       String se= new String(a);
-       return se;
+
+        return new String(a);
     }
+
+    private boolean isVowel(char ch) {
+    // Define the set of vowel characters.
+    char[] vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+
+    // Check if the character is in the set of vowels.
+    for (char vowel : vowels) {
+        if (ch == vowel) {
+            return true;
+        }
+    }
+    return false;
+}
+
 }
