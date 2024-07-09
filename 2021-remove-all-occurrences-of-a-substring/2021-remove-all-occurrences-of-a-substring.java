1899e8e1e378a7ef2619634db57c22a696651b99
Sriraj-03 <rocksri2k@gmail.com>
2024-07-09 11:40:18 +0530

    Time: 33 ms (5.26%), Space: 45.1 MB (7.36%) - LeetHub

---
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,52 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        if (s.length() < part.length()) {
+            return s;
+        }
+        int n = part.length();
+        for (int i = 0; i < s.length(); i++) {
+            String prev = "";
+            String next = "";
+            if (s.length() < part.length()) {
+                return s;
+            }
+            if (i + n > s.length()) {
+                return s;
+            }
+            String curr = customSubstring(s, i, i + n);
+            if (customEquals(curr, part)) {
+                if (i != 0) {
+                    prev = customSubstring(s, 0, i);
+                }
+                if (i != s.length() - 1) {
+                    next = customSubstring(s, i + n, s.length());
+                }
+                s = prev + next;
+                i = -1;
+            }
+        }
+        return s;
+    }
+
+    // Custom method to compare substrings
+    private boolean customEquals(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Custom method to extract substring
+    private String customSubstring(String s, int start, int end) {
+        StringBuilder result = new StringBuilder();
+        for (int i = start; i < end; i++) {
+            result.append(s.charAt(i));
+        }
+        return result.toString();
+    }
+}
