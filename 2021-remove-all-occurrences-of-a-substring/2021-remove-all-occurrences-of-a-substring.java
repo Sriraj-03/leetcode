c2e5458776032703c5814289876d081dd2688b71
Sriraj-03 <rocksri2k@gmail.com>
2024-07-09 11:40:49 +0530

    Time: 33 ms (5.26%), Space: 45.1 MB (7.36%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -50,3 +50,19 @@ class Solution {
         return result.toString();
     }
 }
+// class Solution {
+//     public String removeOccurrences(String s, String part) {
+//         int len = part.length();
+//         while(true) {
+//             int idx = s.indexOf(part);
+
+//             if(idx > -1) {
+//                 s = s.substring(0, idx) + s.substring(idx + len);
+//             } else {
+//                 break;
+//             }
+//         }
+
+//         return s;
+//     }
+// }
