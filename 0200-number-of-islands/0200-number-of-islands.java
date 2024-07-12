23bdeaa3ba83d8c152661f4bb23fbd969c1c34c6
Sriraj-03 <rocksri2k@gmail.com>
2024-07-12 18:53:41 +0530

    Time: 2 ms (99.5%), Space: 49.1 MB (73.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -5,23 +5,27 @@ class Solution {
         int c=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'){
+                if(grid[i][j]=='1')
+                {
                     c++;
-                    mark(grid,i,j,n,m);
+                    helper(grid,i,j,n,m);
                 }
             }
         }
-        return c;
+       
+             return c;
     }
-    void mark(char grid[][],int i,int j,int n,int m){
-        if(i<0 || j<0 || i>=n || j>=m)
+    void  helper(char[][] grid,int i,int j,int n,int m){
+
+        if(i<0|| j<0|| i>=n||j>=m)
         return;
         if(grid[i][j]=='0')
         return;
+
         grid[i][j]='0';
-        mark(grid,i-1,j,n,m);//up
-        mark(grid,i+1,j,n,m);//down
-        mark(grid,i,j-1,n,m);//left
-        mark(grid,i,j+1,n,m);//right
+        helper(grid,i-1,j,n,m);//up
+         helper(grid,i+1,j,n,m);//down
+          helper(grid,i,j-1,n,m);//left
+           helper(grid,i,j+1,n,m);//right
     }
 }
