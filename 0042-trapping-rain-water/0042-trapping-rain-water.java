1b12113f08b1176203a8b495d874fa1146fe60ca
Sriraj-03 <rocksri2k@gmail.com>
2024-07-24 14:59:56 +0530

    Time: 6 ms (11.01%), Space: 44.9 MB (99.2%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,26 +1,50 @@
 class Solution {
     public int trap(int[] height) {
-        int left = 0, right = height.length - 1;
-        int maxLeft = 0, maxRight = 0;
-        int result = 0;
-
-        while (left < right) {
-            if (height[left] <= height[right]) {
-                if (height[left] >= maxLeft) {
-                    maxLeft = height[left];
-                } else {
-                    result += maxLeft - height[left];
-                }
-                left++;
-            } else {
-                if (height[right] >= maxRight) {
-                    maxRight = height[right];
-                } else {
-                    result += maxRight - height[right];
-                }
-                right--;
-            }
+        Stack <Integer> rightmax=new Stack();
+        int leftmax=height[0];
+        int n=height.length;
+        rightmax.push(height[n-1]);
+        for(int i=n-2;i>1;i--){
+            rightmax.push(Math.max(rightmax.peek(),height[i]));
         }
-        return result;
+        int totwater=0;
+        int ans=0;
+        for(int i=1;i<n-1;i++)
+        {
+            int mini=Math.min(leftmax,rightmax.peek());
+            totwater=mini-height[i];
+            totwater=Math.max(0,totwater);
+            ans+=totwater;
+
+            rightmax.pop();
+            leftmax=Math.max(leftmax,height[i]);
+        }
+        return ans;
     }
 }
+// class Solution {
+//     public int trap(int[] height) {
+//         int left = 0, right = height.length - 1;
+//         int maxLeft = 0, maxRight = 0;
+//         int result = 0;
+
+//         while (left < right) {
+//             if (height[left] <= height[right]) {
+//                 if (height[left] >= maxLeft) {
+//                     maxLeft = height[left];
+//                 } else {
+//                     result += maxLeft - height[left];
+//                 }
+//                 left++;
+//             } else {
+//                 if (height[right] >= maxRight) {
+//                     maxRight = height[right];
+//                 } else {
+//                     result += maxRight - height[right];
+//                 }
+//                 right--;
+//             }
+//         }
+//         return result;
+//     }
+// }
