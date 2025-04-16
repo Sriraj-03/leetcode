0fc5438f43928b893a619c83cc3808744056cede
Sriraj-03 <rocksri2k@gmail.com>
2025-04-16 10:28:53 +0530

    Time: 36 ms (73.79%), Space: 44.6 MB (32.47%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,24 +1,42 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-           int n = nums.length;
-	        int[] dp = new int[n];
-	        dp[0] = 1; // Base case: each element is a valid subsequence of length 1
+           int n=nums.length;
+           int[] dp=new int[n];
+           dp[0]=1;
+           for(int i=0;i<n;i++){
+            dp[i]=1;
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j])
+                dp[i]=Math.max(dp[i],dp[j]+1);
+            }
+           }
+           int maxl=0;
+           for(int i:dp)
+           maxl=Math.max(maxl,i);
 
-	        for (int i = 1; i < n; i++) {
-	            dp[i] = 1; // Initialize dp[i] to 1
-	            for (int j = 0; j < i; j++) {
-	                if (nums[i] > nums[j]) {
-	                    dp[i] = Math.max(dp[i], dp[j] + 1);
-	                }
-	            }
-	        }
-
-	        // Find the maximum value in dp[]
-	        int maxLength = 0;
-	        for (int len : dp) {
-	            maxLength = Math.max(maxLength, len);
-	        }
-
-	        return maxLength;
+           return maxl;
 	    }
     }
+
+
+
+// int n = nums.length;
+// 	        int[] dp = new int[n];
+// 	        dp[0] = 1; // Base case: each element is a valid subsequence of length 1
+
+// 	        for (int i = 1; i < n; i++) {
+// 	            dp[i] = 1; // Initialize dp[i] to 1
+// 	            for (int j = 0; j < i; j++) {
+// 	                if (nums[i] > nums[j]) {
+// 	                    dp[i] = Math.max(dp[i], dp[j] + 1);
+// 	                }
+// 	            }
+// 	        }
+
+// 	        // Find the maximum value in dp[]
+// 	        int maxLength = 0;
+// 	        for (int len : dp) {
+// 	            maxLength = Math.max(maxLength, len);
+// 	        }
+
+// 	        return maxLength;
